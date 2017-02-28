16f5458a00c9e9999ba0d5cd5ac8557d6e5a3f1e
Harley Montgomery <harleymontgomery@gmail.com>
2017-02-28 13:51:05 -0800

    correct gameplay logic, hopefully

--- src/engine.py
+++ src/engine.py
@@ -5,6 +5,7 @@ BLACK = 1
 WHITE = -1
 COLUMNS = "ABCDEFGHJKLMNOPQRST"
 
+from bot import Bot
 
 class Engine():
     def __init__(self, size=19):
@@ -24,56 +25,72 @@ class Engine():
         return True
 
     def play(self, move, color):
+        # TODO: Try other data structures, compare speed.
         assert self.legal(move, color)
         # Place the stone and update flags.
         self.board[move] = color
-        self.libs[move] = 4
         self.ko = self.last_move
         self.last_move = move
-        # Flood the loss of liberties.
-        self._flood(self._reduce_libs, move, -color)
-
-    def _flood(self, fn, position, color):
+        # Flood the loss of liberties, keep track of uncounted stones.
         visited = np.zeros((self.size, self.size), dtype=bool)
-        visited[position] = True
+        affected = set([move])
+        self._flood(self._first_sweep, move, color, visited, affected)
+        # Count all of the remaining groups.
+        all_visited = np.zeros((self.size, self.size), dtype=bool)
+        for position in affected:
+            if all_visited[position]:
+                continue
+            visited = np.zeros((self.size, self.size), dtype=bool)
+            counted = set()
+            self._count(position, color, visited, counted)
+            self.libs[visited] = len(counted)
+            all_visited += visited
+
+    def _flood(self, fn, position, *args):
         r, c = position
         if r > 0:
-            fn((r-1, c), color, visited)
+            fn((r-1, c), *args)
         if r < self.size - 1:
-            fn((r+1, c), color, visited)
+            fn((r+1, c), *args)
         if c > 0:
-            fn((r, c-1), color, visited)
+            fn((r, c-1), *args)
         if c < self.size - 1:
-            fn((r, c+1), color, visited)
+            fn((r, c+1), *args)
 
-    def _reduce_libs(self, position, color, visited):
-        """ Reduce liberties of opposite color. """
+    def _first_sweep(self, position, color, visited, affected):
+        if visited[position] or self.board[position] != -color:
+            return
+        if self.libs[position] > 1:
+            self._reduce(position, -color, visited)
+        else:
+            self._remove(position, -color, visited, affected)
+
+    def _reduce(self, position, color, visited):
         if visited[position] or self.board[position] != color:
             return
         visited[position] = True
-        # Reduce liberties.
         self.libs[position] -= 1
-        # Remove dead stones.
-        if self.libs[position] == 0:
-            self.board[position] = EMPTY
-        # Flood forward.
-        r, c = position
-        if r > 0:
-            self._reduce_libs((r-1, c), color, visited)
-        if r < self.size - 1:
-            self._reduce_libs((r+1, c), color, visited)
-        if c > 0:
-            self._reduce_libs((r, c-1), color, visited)
-        if c < self.size - 1:
-            self._reduce_libs((r, c+1), color, visited)
+        self._flood(self._reduce, position, color, visited)
 
-    def _count_libs(self, move, color, visited, libs):
-        if self.board[move] == EMPTY:
-            libs[move] = True
+    def _remove(self, position, color, visited, affected):
+        if visited[position] or self.board[position] == EMPTY:
             return
-        r, c = move
-        visited = np.zeros((self.size, self.size), dtype=bool)
-        libs = np.zeros((self.size, self.size), dtype=bool)
+        if self.board[position] == -color:
+            affected.add(position)
+            return
+        visited[position] = True
+        self.board[position] = EMPTY
+        self.libs[position] = 0
+        self._flood(self._remove, position, color, visited, affected)
+
+    def _count(self, position, color, visited, counted):
+        if visited[position] or self.board[position] == -color:
+            return
+        if self.board[position] == EMPTY:
+            counted.add(position)
+            return
+        visited[position] = True
+        self._flood(self._count, position, color, visited, counted)
 
     def move_from_string(self, string):
         # TODO: error handling
@@ -118,24 +135,33 @@ class Engine():
 
 def interactive_session():
     engine = Engine()
-    from bot import Bot
     bot = Bot(engine, WHITE)
     while True:
         print unicode(engine)
         # Player picks a move.
         string = raw_input('\nMove: ')
-        engine.play(engine.move_from_string(string), BLACK)
+        if string == 'libs':
+            # For debugging.
+            print engine.libs
+            continue
+        try:
+            engine.play(engine.move_from_string(string), BLACK)
+        except:
+            print "Illegal move! Try again."
+            continue
         # Bot responds.
         move = bot.act()
         engine.play(move, WHITE)
 
 if __name__ == "__main__":
-#    interactive_session()
+    interactive_session()
 
-    engine = Engine()
-    engine.play(engine.move_from_string("D4"), WHITE)
-    engine.play(engine.move_from_string("C4"), BLACK)
-    engine.play(engine.move_from_string("D3"), BLACK)
-    engine.play(engine.move_from_string("E4"), BLACK)
-    engine.play(engine.move_from_string("D5"), BLACK)
-    print unicode(engine)
+#    engine = Engine()
+#    engine.play(engine.move_from_string("D4"), WHITE)
+#    engine.play(engine.move_from_string("D5"), WHITE)
+#    engine.play(engine.move_from_string("D3"), BLACK)
+#    engine.play(engine.move_from_string("C4"), BLACK)
+#    engine.play(engine.move_from_string("C5"), BLACK)
+#    engine.play(engine.move_from_string("E4"), BLACK)
+#    engine.play(engine.move_from_string("E5"), BLACK)
+#    print unicode(engine)
