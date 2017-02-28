7288abab60de815d3ef0d2e5ffa2c523a47bb3ae
Harley Montgomery <harleymontgomery@gmail.com>
2017-02-28 02:32:26 -0800

    super basic stone removal

--- src/engine.py
+++ src/engine.py
@@ -13,20 +13,67 @@ class Engine():
         self.libs = np.zeros((self.size, self.size), dtype=int)
         self.last_move = None
         self.ko = None
+        # TODO: keep track of prisoners?
 
     def legal(self, move, color):
-        if move is self.ko:
-            return False
         if self.board[move] != EMPTY:
             return False
-        # TODO:
+        if move is self.ko:
+            return False
+        # TODO: Suicide rule.
         return True
 
     def play(self, move, color):
         assert self.legal(move, color)
+        # Place the stone and update flags.
         self.board[move] = color
+        self.libs[move] = 4
         self.ko = self.last_move
         self.last_move = move
+        # Flood the loss of liberties.
+        self._flood(self._reduce_libs, move, -color)
+
+    def _flood(self, fn, position, color):
+        visited = np.zeros((self.size, self.size), dtype=bool)
+        visited[position] = True
+        r, c = position
+        if r > 0:
+            fn((r-1, c), color, visited)
+        if r < self.size - 1:
+            fn((r+1, c), color, visited)
+        if c > 0:
+            fn((r, c-1), color, visited)
+        if c < self.size - 1:
+            fn((r, c+1), color, visited)
+
+    def _reduce_libs(self, position, color, visited):
+        """ Reduce liberties of opposite color. """
+        if visited[position] or self.board[position] != color:
+            return
+        visited[position] = True
+        # Reduce liberties.
+        self.libs[position] -= 1
+        # Remove dead stones.
+        if self.libs[position] == 0:
+            self.board[position] = EMPTY
+        # Flood forward.
+        r, c = position
+        if r > 0:
+            self._reduce_libs((r-1, c), color, visited)
+        if r < self.size - 1:
+            self._reduce_libs((r+1, c), color, visited)
+        if c > 0:
+            self._reduce_libs((r, c-1), color, visited)
+        if c < self.size - 1:
+            self._reduce_libs((r, c+1), color, visited)
+
+    def _count_libs(self, move, color, visited, libs):
+        if self.board[move] == EMPTY:
+            libs[move] = True
+            return
+        r, c = move
+        visited = np.zeros((self.size, self.size), dtype=bool)
+        libs = np.zeros((self.size, self.size), dtype=bool)
 
     def move_from_string(self, string):
         # TODO: error handling
@@ -69,7 +116,7 @@ class Engine():
         string += "\n   " + " ".join(list(cols))
         return string
 
-if __name__ == "__main__":
+def interactive_session():
     engine = Engine()
     from bot import Bot
     bot = Bot(engine, WHITE)
@@ -81,3 +128,14 @@ if __name__ == "__main__":
         # Bot responds.
         move = bot.act()
         engine.play(move, WHITE)
+
+if __name__ == "__main__":
+#    interactive_session()
+
+    engine = Engine()
+    engine.play(engine.move_from_string("D4"), WHITE)
+    engine.play(engine.move_from_string("C4"), BLACK)
+    engine.play(engine.move_from_string("D3"), BLACK)
+    engine.play(engine.move_from_string("E4"), BLACK)
+    engine.play(engine.move_from_string("D5"), BLACK)
+    print unicode(engine)
