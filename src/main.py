9eb6469001e9c04b924572a5427c02d70292df6b
Harley Montgomery <harleymontgomery@gmail.com>
2017-03-20 19:46:34 -0700

    better argparse

--- src/main.py
+++ src/main.py
@@ -24,19 +24,23 @@ def rollout(engine, black, white, moves=500):
 
 if __name__ == "__main__":
     print "Parsing args"
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--name', default=None)
-    parser.add_argument('-t', '--global_step', default=None)
-    args = parser.parse_args()
+    p = argparse.ArgumentParser()
+    p.add_argument('-n', '--name', default=None)
+    p.add_argument('-t', '--train', action='store_true')
+    p.add_argument('-s', '--global_step', type=int, default=None)
+    p.add_argument('-d', '--data',  default=PRO_H5)
+    p.add_argument('-e', '--epochs', type=int, default=8)
+    args = p.parse_args()
 
     print "Creating bot"
     bot = Bot(name=args.name, global_step=args.global_step)
     engine = Engine()
 
-    print "Loading data"
-    db = h5py.File(PRO_H5, 'r')
-    boards = db["boards"][:]
-    labels = db["labels"][:]
+    if args.train:
+        print "Loading data"
+        db = h5py.File(args.data, 'r')
+        boards = db["boards"][:]
+        labels = db["labels"][:]
 
-    print "Training {}".format(bot.name)
-    bot.train(boards, labels, epochs=8)
+        print "Training {}".format(bot.name)
+        bot.train(boards, labels, epochs=args.epochs)
